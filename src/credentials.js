1827e6eee9ab8e1db95c0ba16e53c457e8f5bce2
Mike <mike@Kates-MacBook-Pro.local>
2020-04-29 11:28:15 -0400

    Remove credentials

--- src/credentials.js
+++
@@ -1,6 +0,0 @@
-const credentials = {
-    clientID: "GGya26lcKzwJ8U1vfr9JZneq0mTDQsmzG9jOBYP7MgGuyqqW06",
-    clientSecret: "tXJlNWpTWSiGDYsmSJBvNQetwRQiaQivjYpnj7fU"
-}
-
-export default credentials;
